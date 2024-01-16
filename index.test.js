3025af5edc8713085356088111cc523fc034822a
am2109a <am2109a>
2024-01-16 21:10:20 +0000

    Linkage between UI and calculate function

--- index.test.js
+++ index.test.js
@@ -12,31 +12,40 @@ describe('Suite to test behaviour of primary school calculation', () => {
     test('that a schol with 100 pupils has an area of 760m^2', () => {
         expect(schoolSizeCalculate(schoolType, 100)).toBe(760)
     });
+    test('that school with 500 pupils has more area than a school with 100 pupils', () => {
+        expect(schoolSizeCalculate(schoolType, 500)).toBeGreaterThan(schoolSizeCalculate(schoolType,100))
+    });
     test('that entering a string as the number of pupil places will return an error', () => {
-        expect(schoolSizeCalculate(schoolType, "testing text")).toBe("Please enter the number of pupil places as a whole number.")
+        expect(schoolSizeCalculate(schoolType, "testing text")).toMatch(/Please enter the number/)
     })
 });
 describe('Suite to test behaviour of secondary up to 16 school calculation', () => {
     let schoolType = 'secondary16';
-    test('that a school with no pupils has an area of 350m^2', () => {
+    test('that a school with no pupils has an area of 1050m^2', () => {
         expect(schoolSizeCalculate(schoolType, 0)).toBe(1050)
     });
-    test('that a schol with 100 pupils has an area of 760m^2', () => {
+    test('that a schol with 100 pupils has an area of 1680m^2', () => {
         expect(schoolSizeCalculate(schoolType, 100)).toBe(1680)
     });
+    test('that school with 500 pupils has more area than a school with 100 pupils', () => {
+        expect(schoolSizeCalculate(schoolType, 500)).toBeGreaterThan(schoolSizeCalculate(schoolType,100))
+    });
     test('that entering a string as the number of pupil places will return an error', () => {
-        expect(schoolSizeCalculate(schoolType, "testing text")).toBe("Please enter the number of pupil places as a whole number.")
+        expect(schoolSizeCalculate(schoolType, "testing text")).toMatch(/Please enter the number/)
     })
 });
 describe('Suite to test behaviour of secondary post 16 school calculation', () => {
     let schoolType = 'secondary16+';
-    test('that a school with no pupils has an area of 350m^2', () => {
+    test('that a school with no pupils has an area of 1400m^2', () => {
         expect(schoolSizeCalculate(schoolType, 0)).toBe(1400)
     });
-    test('that a schol with 100 pupils has an area of 760m^2', () => {
+    test('that a schol with 100 pupils has an area of 2100m^2', () => {
         expect(schoolSizeCalculate(schoolType, 100)).toBe(2100)
     });
+    test('that school with 500 pupils has more area than a school with 100 pupils', () => {
+        expect(schoolSizeCalculate(schoolType, 500)).toBeGreaterThan(schoolSizeCalculate(schoolType,100))
+    });
     test('that entering a string as the number of pupil places will return an error', () => {
-        expect(schoolSizeCalculate(schoolType, "testing text")).toBe("Please enter the number of pupil places as a whole number.")
+        expect(schoolSizeCalculate(schoolType, "testing text")).toMatch(/Please enter the number/)
     })
-})
+});
